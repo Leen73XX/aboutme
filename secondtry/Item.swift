28a58cdfce1acd008eb182306a11a3a4558c7fd5
leen <leen.mej@gmail.com>
2023-10-08 14:41:43 +0300

    Initial Commit

---
+++ secondtry/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  secondtry
+//
+//  Created by Leen Almejarri on 23/03/1445 AH.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
